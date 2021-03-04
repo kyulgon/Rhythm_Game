c129053f91cbe63a9f66d7888fa44f28f9a33be1
unknown <kyulgon@naver.com>
2021-03-04 16:20:28 +0900

    노트 판정

---
+++ Assets/Scripts/Manager/Note.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Note : MonoBehaviour
+{
+    public float noteSpeed = 400;
+
+    Image noteImage;
+
+    private void Start()
+    {
+        noteImage = GetComponent<Image>();
+    }
+
+    public void HideNote() // 처음에 음악소리 재생해주기 위함
+    {
+        noteImage.enabled = false;
+    }
+
+    private void Update()
+    {
+        transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;
+    }
+
+}
---
+++ Assets/Scripts/Manager/NoteManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0; // 분당 비트 나올 수
+    double currentTime = 0d; // 현재 시간
+
+    [SerializeField] Transform tfNoteAppear = null; // note가 나타날 위치
+    [SerializeField] GameObject goNote = null; // note를 담을 변수
+
+    TimingManger theTimingManger;
+
+
+    private void Start()
+    {
+        theTimingManger = GetComponent<TimingManger>();
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime; // 1초에 1씩 증가
+
+        if(currentTime >= 60d / bpm) // 60/120 = 1beat 당 0.5초
+        {
+            GameObject t_note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform); // 캠퍼스 내에 넣어야 note가 보이므로 SetParent함
+
+            theTimingManger.boxNoteList.Add(t_note); // List에 t_note 넣어줌
+            currentTime -= 60d / bpm; // currentTime = 0 안됨(0.01005~정도의 시간만큼 오차가 손실됨)
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Note"))
+        {
+            theTimingManger.boxNoteList.Remove(collision.gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
